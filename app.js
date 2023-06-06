50e3292dfaa90ad74d934dc6eb8faa2f32313403
olawills <wobdele@gmail.com>
2023-06-06 23:16:18 +0100

    corrected app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-const server = app.listen(PORT, "0.0.0.0", () => {
+const server = app.listen(PORT, () => {
   console.log("Server has started on ", PORT);
 });
 
