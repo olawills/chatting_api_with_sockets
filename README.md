d0fdd301cb804b11dcaf6b84c4b66a491ead9016
olawills <wobdele@gmail.com>
2023-06-06 08:47:04 +0100

    chatting api with scokets

---
+++ README.md
@@ -0,0 +1,14 @@
+## Chatting API With SOCKET.IO
+
+<p>There is only one file in this repo named app.js</p>
+<p>>this file shows a simple implementation of how to use socket and express js</p>
+<p>The file basically connects to the socket and within each connection it contains the socket connection for both the messenger and the receiver</p>
+
+<h3>Future Feature to come to the API</h3>
+
+<ul>
+    <p> Structure to the Files</p>
+    <p> Connect to Database with MONGO DB</p>
+    <p> Add Group Chatting </p>
+    <p> Save users to Database </p>
+</ul>
---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+const server = app.listen(PORT, () => {
+  console.log("Server has started on ", PORT);
+});
+
+const connectedUser = new Set();
+// connecting the socket to our server
+
+const io = require("socket.io")(server);
+
+io.on("connection", (socket) => {
+  console.log("connected successfully", socket.id);
+  connectedUser.add(socket.id);
+  io.emit("connected-user", connectedUser.size);
+  socket.on("disconnected", (socket) => {
+    console.log("Disconnected", socket.id);
+    connectedUser.delete(socket.id);
+    io.emit("connected-user", connectedUser.size);
+  });
+  socket.on("message", (data) => {
+    console.log(data);
+    socket.broadcast.emit("message-receive", data);
+  });
+});
